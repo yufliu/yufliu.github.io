0a147f7b8c85fb253d410b52da687821e3f3278f
Yu Liu <yuliu1996@yahoo.com>
2020-10-26 13:34:47 -0400

    test

---
+++ contactform.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "liuy0311@ufl.edu";
+    $headers = "From: ".$mailFrom
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
+
+
+
+
+
+ ?>
