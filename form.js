b6f191bda0e0c001b8bc6ea7098e2d6e02aaeb1c
Yu Liu <yuliu1996@yahoo.com>
2020-10-26 03:34:45 -0400

    php fix?
    forms.js: contact.php --> handler.php
    added handler.php to main dir

--- form.js
+++ form.js
@@ -53,7 +53,7 @@ $(function()
 
                     $.ajax({
                 type: "POST",
-                url: 'contact.php',
+                url: 'handler.php',
                 data: $form.serialize(),
                 success: after_form_submitted,
                 dataType: 'json'
