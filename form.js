ed973cd3ee219bf7c17d137398e6c58307700acc
Yu Liu <yuliu1996@yahoo.com>
2020-10-26 03:48:58 -0400

    Update form.js

--- form.js
+++ form.js
@@ -53,7 +53,7 @@ $(function()
 
                     $.ajax({
                 type: "POST",
-                url: 'handler.php',
+                url: './forms/contact.php',
                 data: $form.serialize(),
                 success: after_form_submitted,
                 dataType: 'json'
