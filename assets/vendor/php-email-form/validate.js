030dab38e1b5ea1d3525c91632b3e84d379826e4
Yu Liu <yuliu1996@yahoo.com>
2020-10-26 15:41:01 -0400

    b

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,15 +6,15 @@
 !(function($) {
   "use strict";
 
-  $('form.php-email-form').submit(function(e) {
+  $('form.php-email-formz').submit(function(e) {
     e.preventDefault();
-    
+
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
     f.children('input').each(function() { // run all inputs
-     
+
       var i = $(this); // current input
       var rule = i.attr('data-rule');
 
@@ -104,7 +104,7 @@
       this_form.find('.error-message').slideDown().html('The form action property is not set!');
       return false;
     }
-    
+
     this_form.find('.sent-message').slideUp();
     this_form.find('.error-message').slideUp();
     this_form.find('.loading').slideDown();
@@ -119,7 +119,7 @@
     } else {
       php_email_form_submit(this_form,action,this_form.serialize());
     }
-    
+
     return true;
   });
 
